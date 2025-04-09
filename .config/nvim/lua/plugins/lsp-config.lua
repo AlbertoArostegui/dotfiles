f5e9f012cd8c85bb74894a68131fd4dca225aadf
AlbertoArostegui <aarostegui@gmail.com>
2025-04-09 22:44:29 +0200

    Verible on LSP config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", autoinstall = true },
+				ensure_installed = { "lua_ls", "clangd", "verible", autoinstall = true },
 			})
 		end,
 	},
