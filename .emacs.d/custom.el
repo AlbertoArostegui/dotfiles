ed035c5711e7a7c50c0bb8aee778b39d8be74be9
AlbertoArostegui <aarosteguig@gmail.com>
2025-05-10 00:03:35 +0200

    Emacs config (thanks David)

---
+++ .emacs.d/custom.el
@@ -0,0 +1,42 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-backends
+   '(company-dabbrev-code company-semantic company-cmake company-capf
+                          company-clang company-files
+                          (company-dabbrev-code company-gtags
+                                                company-etags
+                                                company-keywords)
+                          company-oddmuse company-dabbrev))
+ '(custom-enabled-themes '(gruber-darker))
+ '(custom-safe-themes
+   '("b4b5da90759ab14719f1e1d8d0138ff72d2901e8a63748b172944627513cfffb"
+     "01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
+     "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+     default))
+ '(evil-undo-system 'undo-tree)
+ '(indent-tabs-mode nil)
+ '(package-selected-packages
+   '(amx cider clojure-mode company counsel diff-hl evil evil-collection
+         gruber-darker-theme haskell-mode helpful magit mood-line
+         smooth-scroll smooth-scrolling timu-macos-theme undo-tree
+         vterm vterm-toggle))
+ '(verilog-auto-lineup 'ignore)
+ '(verilog-auto-newline nil)
+ '(verilog-case-indent 4)
+ '(verilog-cexp-indent 4)
+ '(verilog-indent-level 4)
+ '(verilog-indent-level-behavioral 4)
+ '(verilog-indent-level-declaration 4)
+ '(verilog-indent-level-module 4)
+ '(whitespace-action '(auto-cleanup)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-blue ((t (:background "dodger blue" :foreground "dodger blue"))))
+ '(mode-line-active ((t (:inherit mode-line :box (:line-width (2 . 2) :color "gold" :style released-button))))))
+(set-face-attribute 'default nil :height 130)
---
+++ .emacs.d/init.el
@@ -0,0 +1,98 @@
+;; Custom file
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+;; Real important - Font
+(set-frame-font "Iosevka Nerd Font Mono 14" nil t)
+
+;; Melpa
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; Install packages
+(package-install-selected-packages)
+
+;; Lockfiles
+(setq auto-save-default nil
+      create-lockfiles nil
+      delete-old-versions -1
+      vc-make-backup-files t)
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+
+;; Window modes
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+;; Keybindings - Vim motions
+(setq evil-want-C-u-scroll t)
+(setq evil-want-keybinding 'nil)
+(evil-mode)
+(evil-collection-init)
+
+(global-set-key (kbd "C-x C-l") #'toggle-truncate-lines) 
+(global-set-key (kbd "<f5>") #'revert-buffer-quick)
+
+;; Disable splash screen
+(setq inhibit-startup-message t) 
+(setq initial-scratch-message nil)
+
+;; Mood Line
+(mood-line-mode 1)
+
+;; Relative lines
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
+
+;; Smooth scroll
+(require 'smooth-scrolling)
+(smooth-scrolling-mode 1)
+
+;; Undo tree
+(setq undo-tree-visualizer-timestamps t
+      undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo-tree-files")))
+(global-undo-tree-mode)
+
+;; Magit
+(global-set-key (kbd "C-x g") 'magit-status)
+(setq evil-magit-state 'motion)
+
+;; Vterm
+(setq async-shell-command-display-buffer nil)
+(setq vterm-toggle-fullscreen-p nil)
+(setq vterm-toggle-scope 'project)
+(add-to-list 'display-buffer-alist
+	     '((lambda(bufname _) (with-current-buffer bufname (equal major-mode 'vterm-mode)))
+	       (display-buffer-reuse-window display-buffer-at-bottom)
+	       (direction . bottom)
+	       (dedicated . t) ;dedicated is supported in emacs27
+	       (reusable-frames . visible)
+	       (window-height . 0.3)))
+
+(global-set-key (kbd "C-x C-<return>") 'vterm-toggle)
+(put 'downcase-region 'disabled nil)
+
+;; Helpful
+(global-set-key (kbd "C-h f") 'helpful-function)
+(global-set-key (kbd "C-h v") 'helpful-variable)
+(global-set-key (kbd "C-h k") 'helpful-key)
+
+;; Completion
+(global-set-key (kbd "C-SPC")   'counsel-M-x)
+(global-set-key (kbd "C-x C-b") 'counsel-switch-buffer)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-s")     'swiper-isearch-thing-at-point)
+
+(ivy-mode 1)
+(setq ivy-count-format " [%d/%d] "
+      ivy-wrap t)
+
+;; Programming
+(setq-default indent-tabs-mode nil)
+(global-company-mode)
+
+;; ediff
+(setq ediff-window-setup-function #'ediff-setup-windows-plain
+      ediff-split-window-function #'split-window-horizontally)
