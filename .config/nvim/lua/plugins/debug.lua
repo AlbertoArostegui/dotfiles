ae46de313bdd41432a67c25cf89052d09abd2a96
AlbertoArostegui <aarosteguig@gmail.com>
2026-04-09 10:29:08 +0200

    Debugger in nvim

---
+++ .config/nvim/lua/plugins/debug.lua
@@ -0,0 +1,58 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "theHamsta/nvim-dap-virtual-text",
+    "williamboman/mason.nvim",
+    "jay-babu/mason-nvim-dap.nvim",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    require("mason-nvim-dap").setup({
+      ensure_installed = { "codelldb" },
+      automatic_installation = true,
+    })
+
+    local codelldb_path = vim.fn.stdpath("data") .. "/mason/bin/codelldb"
+    dap.adapters.codelldb = {
+      type = "server",
+      port = "${port}",
+      executable = {
+        command = codelldb_path,
+        args = { "--port", "${port}" },
+      },
+    }
+
+    dap.configurations.cpp = {
+      {
+        name = "Launch",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Ejecutable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+      },
+    }
+    dap.configurations.c = dap.configurations.cpp
+
+    -- UI
+    dapui.setup()
+    require("nvim-dap-virtual-text").setup()
+
+    dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+    dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+    dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+
+    vim.keymap.set("n", "<F5>", dap.continue)
+    vim.keymap.set("n", "<F10>", dap.step_over)
+    vim.keymap.set("n", "<F11>", dap.step_into)
+    vim.keymap.set("n", "<F12>", dap.step_out)
+    vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+    vim.keymap.set("n", "<leader>du", dapui.toggle) -- toggle manual de la UI
+  end,
+}
